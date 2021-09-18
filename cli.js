986afdf35e494ef25eb8c5af4492b473abb2aa50
sporeball <sporeballdev@gmail.com>
2021-09-17 19:50:21 -0700

    order properties

--- cli.js
+++ cli.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import { order } from './lib/order.js';
+
 import fs from 'fs';
 import eol from 'eol';
 import chalk from 'chalk';
@@ -15,6 +17,8 @@ const args = yeow({
   }
 });
 
+const RULE_REGEX = /[*\w.#()\[\]=~|^$, >+:-]+{[\w\n -]+:.+?}/gs;
+
 let source;
 try {
   source = eol.lf(fs.readFileSync(args.file, { encoding: 'utf-8' }));
@@ -23,11 +27,25 @@ try {
   process.exit(1);
 }
 
-const rules = source.match(/(\w| )+{.+?}/gs);
+const rules = source.match(RULE_REGEX);
 for (const rule of rules) {
-  const props = rule.split('\n')
-    .map(line => line.trim())
+  const lines = rule.split('\n');
+  const selector = lines[0].slice(0, lines[0].indexOf('{')).trim();
+  const props = lines.map(line => line.trim())
     .filter(line => line.match(/^[^/*]+?:[^;]+?;?$/))
     .map(prop => prop.slice(0, prop.indexOf(':')));
+
+  props.sort((a, b) => {
+    a = order.indexOf(a);
+    b = order.indexOf(b);
+    if (a === -1) {
+      a = 999;
+    }
+    if (b === -1) {
+      b = 999;
+    }
+    return a - b;
+  });
+
   console.log(props);
 }
